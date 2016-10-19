5a503ca9094cc586e60bf01329ee10bd372d332e
Sahbana <sahbanalo@gmail.com>
2016-10-19 14:51:10 +0700

    build test travisci

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
+var mocha = require('mocha')
+var chai = require('chai')
 var dmy = require('./index.js')
+var should = chai.should();
 
-console.log(dmy("dmy","m"));
-console.log(dmy())
-console.log(dmy("dmy"))
-console.log(dmy("dmy","m"))
-console.log(dmy("mdy"))
-console.log(dmy("mdy","m"))
+
+describe('dmy', function() {
+    it('should return date of today', function(done) {
+      console.log(dmy());
+        dmy().should.equal(new Date().getDate()+"/"+(new Date().getMonth()+1)+"/"+(new Date().getYear()+1900))
+                done();
+    });
+})
